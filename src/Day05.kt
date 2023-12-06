6d34879cc0639673caa44302f5f04c4fbd832976
road <rod8711@gmail.com>
2023-12-06 16:06:49 +0700

    simplify Day05

--- src/Day05.kt
+++ src/Day05.kt
@@ -1,5 +1,3 @@
-import kotlin.math.min
-
 /**
  * You can edit, run, and share this code.
  * play.kotlinlang.org
@@ -8,95 +6,73 @@ fun main() {
     val testInput = readInput("Day05_test")
     val input = readInput("Day05")
 
-    data class Mapping(val destStart: Long, val srcStart: Long, val length: Long) {
+    data class MapRule(val destStart: Long, val srcStart: Long, val length: Long) {
         fun mapOrNull(number: Long): Long? {
-            if (srcStart <= number && number <= srcStart + length - 1) {
-                return number + destStart - srcStart
+            return if (srcStart <= number && number <= srcStart + length - 1) {
+                number + destStart - srcStart
+            } else {
+                null
             }
-            return null
         }
 
-        fun inverse(): Mapping {
-            return Mapping(srcStart, destStart, length)
+        fun inverse(): MapRule {
+            return MapRule(srcStart, destStart, length)
         }
     }
 
-    // apply list of mapping rules (e.g. seed-to-soil map) in to a value
-    fun applyMapping(value: Long, maps: List<Mapping>): Long {
-        maps.forEach {
-            val mapped = it.mapOrNull(value)
-            if (mapped != null) {
-                return mapped
-            }
-        }
-        return (value)
+    fun List<MapRule>.map(value: Long): Long {
+        return this.firstNotNullOfOrNull { it.mapOrNull(value) } ?: value
     }
 
-    fun part1(input: List<String>): Long {
+    fun List<List<MapRule>>.map(value: Long): Long {
+        return this.fold(value) { acc, layer -> layer.map(acc) }
+    }
+
+
+    fun parseInput(input: List<String>): Pair<List<Long>, List<List<MapRule>>> {
         val chunks = input.windowWhen { it == "" }
         val seedLine = chunks.first().first()
         val seeds = seedLine.substringAfter(": ").split(" ").map { it.toLong() }
+        // layer of maps, i.e. listOf(seed-to-soil, soil-to-fertilizer, ...)
         val mapLayers = chunks.drop(1).map {
             val mapLines = it.drop(1)
-            val mapData = mapLines.map { it.split(" ").map { it.toLong() }.let { (a, b, c) -> Mapping(a, b, c) } }
+            val mapData = mapLines.map { it.split(" ").map { it.toLong() }.let { (a, b, c) -> MapRule(a, b, c) } }
             mapData
         }
 
-
-        val locations = seeds.map {
-            mapLayers.fold(it, { acc, mapLayer -> applyMapping(acc, mapLayer) })
-        }
-
-        return locations.min()
+        return Pair(seeds, mapLayers)
     }
 
-    fun applyMultiLayers(value: Long, layers: List<List<Mapping>>): Long {
-        return layers.fold(value) { acc, it -> applyMapping(acc, it ) }
+    fun part1(input: List<String>): Long {
+        val (seeds, mapLayers) = parseInput(input)
+        return seeds.minOf { mapLayers.map(it) }
     }
+
 
     fun part2(input: List<String>): Long {
-        val chunks = input.windowWhen { it == "" }
-        val seedLine = chunks.first().first()
-        val seedValues = seedLine.substringAfter(": ").split(" ").map { it.toLong() }
-        val mapLayers = chunks.drop(1).map {
-            val mapLines = it.drop(1)
-            val mapData = mapLines.map { it.split(" ").map { it.toLong() }.let { (a, b, c) -> Mapping(a, b, c) } }
-            mapData
-        }
+        val (seeds, mapLayers) = parseInput(input)
 
-        val boundaryPoints = mutableSetOf<Long>()
-        val reverseMapLayers = mapLayers.reversed().map { layer -> layer.map { it.inverse() }}
-        // calculate boundary point for all layers
-        reverseMapLayers.forEachIndexed { index, currentLayer ->
-            val restLayers = reverseMapLayers.drop(index + 1)
-            // calculate boundary point for each layer
-            currentLayer.forEach {
-                val src = it.destStart
-                val origToAchieve = restLayers.fold(src, { acc, layer -> applyMapping(acc, layer)})
-                boundaryPoints.add(origToAchieve)
-            }
-        }
-
-        var minLoc = Long.MAX_VALUE
-        seedValues.chunked(2).forEach { (start, length) ->
-            val seedRange = start .. (start + length - 1)
-            val locStart = min(minLoc, applyMultiLayers(start, mapLayers))
-            if (locStart < minLoc) {
-                minLoc = locStart
-            }
-
-            boundaryPoints.forEach {
-                if (seedRange.contains(it)) {
-                    val locInteresting = min(minLoc, applyMultiLayers(it, mapLayers))
-                    if (locInteresting < minLoc) {
-                        minLoc = locInteresting
-                    }
+        val boundaryPoints = buildSet {
+            val inverseMapLayers = mapLayers.reversed().map { layer -> layer.map { it.inverse() } }
+            // calculate boundary point for all layers
+            inverseMapLayers.forEachIndexed { index, currentLayer ->
+                val restLayers = inverseMapLayers.drop(index + 1)
+                // calculate boundary point for each layer
+                currentLayer.forEach {
+                    val src = it.destStart
+                    val origToAchieve = restLayers.map(src)
+                    add(origToAchieve)
                 }
             }
+
         }
 
+        val seedsToCheck = seeds.chunked(2).flatMap { (start, length) ->
+            val seedRange = start..(start + length - 1)
+            boundaryPoints.filter { seedRange.contains(it) } + seedRange.start
+        }
 
-        return minLoc
+        return seedsToCheck.minOf { mapLayers.map(it) }
     }
 
     assertEqual(part1(testInput), 35)
